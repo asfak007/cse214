5336c8e36b4fa5772b6b983b94ca41023d23ea00
1834902575 <65647153+1834902575@users.noreply.github.com>
2020-06-06 22:08:02 +0600

    Create 8.c

---
+++ lab3/8.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int i, n,array[100];
+
+    printf("How many Fibonacci numbers : ");
+    scanf("%d",&n);
+
+    array[0]=0;
+    array[1]=1;
+
+    for(i=2;i<n;i++)
+        {
+           array[i]=array[i-1] +array[i-2];
+
+        }
+
+    for(i=0;i<n;i++)
+    {
+        printf("%d\n ",array[i]);
+    }
+
+     return 0;
+}
