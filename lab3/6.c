38d68a9f5b9d42aa9e0986d3602869b5809d5c9e
1834902575 <65647153+1834902575@users.noreply.github.com>
2020-06-06 22:07:14 +0600

    Create 6.c

---
+++ lab3/6.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int array[5]={1,2,3,4,5};
+    int i,sum=0,avg;
+    printf("Given Array : ");
+    for(i=0;i<5;i++)
+    {
+        printf("%d ",array[i]);
+    }
+
+    for(i=0;i<5;i++)
+    {
+        sum = sum+array[i];
+    }
+
+    avg = sum/5;
+
+    printf("\nAverage Of this Array is : %d",avg);
+    return 0;
+}
+
