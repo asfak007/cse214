4a70a3990386d7fe68f685e69aec9341582bee94
1834902575 <65647153+1834902575@users.noreply.github.com>
2020-06-06 22:04:26 +0600

    Create 2.c

---
+++ lab3/2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int array[5]={1,2,3,4,5};
+    int i, min;
+
+     printf("Hole Array :\n");
+     for(i=0;i<5;i++)
+     {
+         printf("%d ",array[i]);
+     }
+     min = array[0];
+
+     for(i=1;i<5;i++)
+     {
+         if(min>array[i])
+         {
+             min=array[i];
+         }
+     }
+     printf("\nMinimum value : %d  ",min);
+
+    return 0;
+}
