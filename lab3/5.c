841855d8df78373b8b21a86aea9c4a420496286f
1834902575 <65647153+1834902575@users.noreply.github.com>
2020-06-06 22:06:37 +0600

    Create 5.c

---
+++ lab3/5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int array[5]={1,2,3,4,5};
+    int i,sum=0;
+    printf("Given Array : ");
+    for(i=0;i<5;i++)
+    {
+        printf("%d ",array[i]);
+    }
+
+    for(i=0;i<5;i++)
+    {
+        sum = sum+array[i];
+    }
+
+    printf("\nSummation Of this Array Is : %d",sum);
+    return 0;
+}
