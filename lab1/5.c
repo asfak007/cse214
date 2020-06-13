2f1977c287226b5975f56db7270ff9d61250ddf9
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 11:06:30 +0600

    Create 5.c

---
+++ lab1/5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+
+    int arr[100] = {1, 2, 3, 4, 5, 6, 7,8, 9, 10};
+    int size = 10, i;
+    
+    for(i = 0; i<size; i++){
+    
+        printf("%d ", array[i]);
+    }
+    return 0;
+}
