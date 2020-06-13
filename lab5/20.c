da24db586d09ce39105e23aeb28f0923d68dccac
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 13:15:08 +0600

    Create 20.c

---
+++ lab5/20.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    int i;
+    int *p;
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = 10;
+
+    p = &arr[0];
+    
+    for(i=0; i<size; i++){
+        printf("%d ",*(p+i));
+     }
+    printf("\n");
+
+    *(p+size) = *p;
+
+    for (i=0; i<size; i++){
+        *(p+i) = *(p+i+1);
+    }
+    
+    for(i=0; i<size; i++){
+        printf("%d ",*(p+i));
+    }
+    return 0;
+}
