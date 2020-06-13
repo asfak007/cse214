e6cc4044274cabb1904d3b4a7446fcc3e6b86124
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 13:01:13 +0600

    Create 15.c

---
+++ lab5/15.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int i;
+    int *p, *q;
+    int temp = 0;
+    int arr1[100] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int arr2[10];
+    int size = 10;
+
+    p = &arr1[0];
+    q = &arr2[0];
+
+    for(i=0; i<size; i++){
+        printf("%d ",*(p+i));
+        }
+
+    printf("\n");
+
+    for(i=0; i<=size; i++){
+        tag = *(p+size-i);
+        *(q+i-1) = tag;
+    }
+
+    for(i=0; i<size; i++){
+        printf("%d ",*(q+i));
+     }
+    return 0;
+}
