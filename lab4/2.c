7ed42eebd6cea1b27017ff2dd92be0a696006797
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 11:38:07 +0600

    Create 2.c

---
+++ lab4/2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+
+
+    int i;
+    int temp = 0;
+    int arr1[100] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int arr2[10];
+    int size = 10;
+
+    for(i=0; i<size; i++){
+        printf("%d ",arr1[i]);
+     }
+    printf("\n");
+
+    for(i=0; i<=size; i++){
+        temp = arr1[size-i];
+        array2[i-1] = temp;
+    }
+
+    for(i=0; i<size; i++){
+        printf("%d ",arr2[i]);
+     }
+    return 0;
+}
