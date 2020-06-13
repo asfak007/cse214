8de4e42785d3f7e3d731cc385e2ccf925c3b6193
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 12:11:03 +0600

    Create 4.c

---
+++ lab5/4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    int *p;
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = 10;
+
+    p = &arr[0];
+
+    for(i=0; i<size; i++){
+        printf("%d ", *(p+i));
+        }
+
+    return 0;
+}
