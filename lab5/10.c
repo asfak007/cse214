9b6d3585b0e198716dac53bde3d6cd9c5d6169aa
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 12:50:35 +0600

    Create 10.c

---
+++ lab5/10.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+
+    int i;
+    int *min;
+    int arr[100] = {9, 78, 67, 85, 96, 7, 12, 2, 63, 5};
+    int size = 10;
+
+    min = &arr[0];
+
+    for(i=0; i<size; i++){
+        printf("%d ",*(min+i));
+        }
+
+    printf("\n");
+
+    for(i=1; i<size; i++){
+        if(*(min+i)<*min)
+            *min = *(min+i);
+            }
+
+    printf("%d\n",*min);
+
+    return 0;
+}
