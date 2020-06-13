f6dfa33fcd29b61a90091c907f6efb1107414fb8
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 13:34:38 +0600

    Update 1.c

--- lab2/1.c
+++ lab2/1.c
@@ -1,21 +1,18 @@
  #include<stdio.h>
- int main()
- {
-     int array[11]={1,2,3,4,5,6,7,8,9,10,11};
+ int main(){
+     int arr[11]={1,2,3,4,5,6,7,8,9,10,11};
 
      printf("Original Array : \n");
 
      int i;
-     for(i=0;i<11;i++)
-        {
-          printf("%d ",array[i]);
+     for(i=0;i<11;i++){
+          printf("%d ",arr[i]);
         }
         array[10] = 123 ;
 
      printf("\nAfter INSERT value: \n ");
-     for(i=0;i<11;i++)
-        {
-          printf("%d ",array[i]);
+     for(i=0;i<11;i++){
+          printf("%d ",arr[i]);
         }
 
      return 0 ;
