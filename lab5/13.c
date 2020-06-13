63943fbfe94b085775b920e10d030d566d88bef3
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 12:59:24 +0600

    Create 13.c

---
+++ lab5/13.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+
+    int i;
+    int *p;
+    int sum = 0;
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = 10;
+
+    p = &array[0];
+
+    for(i=0; i<size; i++){
+        printf("%d ",*(p+i));
+        }
+
+    printf("\n");
+
+    for(i=0; i<size; i++){
+        sum += *(p+i);
+        }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
