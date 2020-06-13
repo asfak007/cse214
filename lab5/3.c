792f9de5a1fb7d00045bf7e5a1875351dd06b928
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 12:09:52 +0600

    Create 3.c

---
+++ lab5/3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+
+    int *p;
+    int i, size;
+    int ar[100];
+
+    p = &ar[0];
+
+    for(i=0; scanf("%d",(p+i))!=EOF; i++)
+        if(*(p+i)==0)
+            break;
+
+    size=i;
+
+    printf("\n");
+
+    for(i=0; i<size; i++){
+        printf("%d ",*(p+i));
+    }
+    return 0;
+}
