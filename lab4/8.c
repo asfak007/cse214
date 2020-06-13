fd2b9ea1c5d6634e94bea0b1a9f53a992b54993e
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 11:58:35 +0600

    Create 8.c

---
+++ lab4/8.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    int i;
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = 10;
+
+    for (i=0; i<size; i++){
+        printf("%d ",arr[i]);
+        }
+
+    printf("\n");
+
+    arr[size] = arr[0];
+
+    for (i=0; i<size; i++){
+        arr[i] = arr[i+1];
+        }
+
+    for (i = 0; i<size; i++){
+        printf ("%d ", arr[i]);
+        }
+
+    return 0;
+}
