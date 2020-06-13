683900ef48574ab22849ed7257ec5a97768cb9f9
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 11:40:31 +0600

    Create 3.c

---
+++ lab4/3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+
+
+    int i, j, temp;
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = 10;
+
+    for(i=0; i<size; i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+
+    for(i=size-1, j=0; j<i; i--, j++){
+        temp = arr[j];
+        array[j] = arr[i];
+        array[i] = temp;
+    }
+
+    for(i=0; i<size; i++){
+        printf("%d ", arr[i]);
+     }
+    return 0;
+}
