7bfaa25da0fe9f38253835bb959e86c89c5ab426
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 13:05:59 +0600

    Create 14.c

---
+++ lab5/14.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+
+    int i;
+    int *p, *q;
+    int temp = 0;
+    int arr1[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int arr2[10];
+    int size = 10;
+
+    p = &arr1[0];
+    q = &arr2[0];
+
+    for(i=0; i<size; i++){
+        printf("%d ",*(p+i));
+    }
+    printf("\n");
+
+    for(i=0; i<size; i++){
+        temp = *(p+i);
+        *(q+i) = temp;
+    }
+
+    for(i=0; i<size; i++){
+        printf("%d ",*(q+i));
+     }
+    return 0;
+}
