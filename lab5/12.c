500de215b06fe488eb51622b864a8a373954b545
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 12:57:54 +0600

    Create 12.c

---
+++ lab5/12.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int *min;
+    int i, min2;
+    int array[10] = {3, 8, 87, 489, 383, 7, 6, 253, 9, 953};
+    int size = 10;
+
+    min = &arr[0];
+
+    for(i=0; i<size; i++){
+        printf("%d ",*(min+i));
+      }
+      
+    printf("\n");
+
+    for(i=0; i<size; i++){
+    
+        if(*(min+i)<*min){
+        
+            min2 = *min;
+            *min = *(min+i);
+        }
+        
+        else if(*(min+i)<min2 && *(min+i)!=*min){
+            min_2nd = *(min+i);
+            }
+    }
+
+    printf("%d\n", min2);
+
+    return 0;
+}
