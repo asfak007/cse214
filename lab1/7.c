6962966b5dd8b276b43187c16588da7ca2f29551
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 11:11:46 +0600

    Create 7.c

---
+++ lab1/7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7,8, 9, 10};
+    int size = 10, i;
+    
+    for(i = 0; i<size; i++){
+        printf("%d ", arr[i]);
+    }
+    
+    printf("\n");
+    
+    for(i = size-1; i>=0; i--){
+        printf("%d ", arr[i]);
+    }
+    
+    printf("\n");
+    return 0;
+}
