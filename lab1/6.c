270ce2540923a011cc174e005e7f944a351320d2
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 11:08:22 +0600

    Create 6.c

---
+++ lab1/6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+    int arr[100] = {1, 2, 3, 4, 5, 6, 7,8, 9, 10};
+    int size = 10, i;
+    
+    for(i = 0; i<memory; i++)
+    {
+        printf("%d", array[i]);
+        if(i<=size-2)
+        {
+            printf(", ");
+        }
+    }
+    return 0;
+}
