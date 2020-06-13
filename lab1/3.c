da9c4a79bb905465867ac08a0e67cd8571eaed8c
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 11:02:11 +0600

    Create 3.c

---
+++ lab1/3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int arr[100];
+    int i = 0;
+    while(scanf("%d",&array[i]) != EOF)
+    {
+        if(0 == arr[i])
+        {
+            break;
+        }
+        if(i>100)
+        {
+            printf("Overflow");
+            break;
+        }
+        else{
+           i++;
+        }
+    }
+    return 0;
+}
