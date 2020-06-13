5527b95ff417202dd0af7c3ffed1076d93c32b17
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 12:07:19 +0600

    Create 2.c

---
+++ lab5/2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+
+    int *p;
+    int i;
+    int arr[100];
+    int size = 0;
+
+    p = &arr[0];
+
+    for(i=0; scanf("%d",(p+i))!=EOF; i++){
+        size++;
+        }
+
+    printf("\n");
+
+    for(i=0; i<size; i++){
+        printf("%d ",*(p+i));
+        }
+
+    return 0;
+}
