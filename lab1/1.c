4f1fd5e5e161b1c6ba643c5849b5f5dff4059045
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 10:58:11 +0600

    Create 1.c

---
+++ lab1/1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i, j;
+    int arr[100];
+    scanf("%d",&n);
+    if(n>100)
+    {
+        printf("Overflow");
+    }
+    else
+    {
+        for(j = 0; j<n; j++)
+        {
+            scanf("%d",&arr[j]);
+        }
+    }
+    return 0;
+}
