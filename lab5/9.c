aba5a273630fcad60bc930612303892189241df7
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 12:48:45 +0600

    Create 9.c

---
+++ lab5/9.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+
+    int i;
+    int *max;
+    int arr[10] = {7, 0, 9, 74, 39, 1, 70, 2, 66, 5};
+    int size = 10;
+
+    max = &array[0];
+
+    for(i=0; i<size; i++){
+        printf("%d ", *(max+i));
+     }
+    printf("\n");
+
+    for(i=1; i<size; i++){
+        if(*(max+i)>*max)
+            *max = *(max+i);
+            }
+
+    printf("%d\n",*max);
+
+    return 0;
+}
