5584bcd1cf1f9d70e5185fc831e1c37cdcb234e7
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 12:55:05 +0600

    Create 11.c

---
+++ lab5/11.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+
+    int *max;
+    int i, max2;
+    int arr[100] = {3, 69, 68, 45, 26, 78, 52,4, 9, 71};
+    int size = 10;
+
+    max = &array[0];
+
+    for(i=0; i<size; i++){
+        printf("%d ",*(max+i));
+       }
+    printf("\n");
+
+    for(i=0; i<size; i++){
+    
+        if(*(max+i)>*max){
+        
+            max_2nd = *max;
+            *max = *(max+i);
+        }
+        else if(*(max+i)>max2 && *(max+i)<*max){
+            max2 = *(max+i);
+            }
+    }
+
+    printf("%d\n", max2);
+
+    return 0;
+}
