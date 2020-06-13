c238ef42186a872d658e19b50e0473625fbb5c24
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-13 11:00:10 +0600

    Create 2.c

---
+++ lab1/2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100];
+    int i = 0;
+    while(scanf("%d",&arr[i]) != EOF)
+    {
+        i++;
+        if(i+1>100)
+        {
+            printf("Overflow");
+            break;
+        }
+    }
+    return 0;
+}
