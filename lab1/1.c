2f1388217c5ee67ed1c0075f3a7cca195d9b8eb8
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-15 04:08:37 +0600

    Update 1.c

--- lab1/1.c
+++ lab1/1.c
@@ -1,18 +1,17 @@
-/*Scan  values from user into an array*/
-
+/*Scan N (take input N from user) value from user into an array*/
 #include<stdio.h>
 int main()
 {
-    int i, j;
+    int i, j,N;
     int arr[100];
-    scanf("%d",&n);
+    scanf("%d",&N);
     if(n>100)
     {
         printf("Overflow");
     }
     else
     {
-        for(j = 0; j<n; j++)
+        for(j = 0; j<N; j++)
         {
             scanf("%d",&arr[j]);
         }
