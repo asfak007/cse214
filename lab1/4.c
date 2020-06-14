c4a9c00fd34481679e3d0fc791d1f184a6bae07b
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-15 04:12:40 +0600

    Update 4.c

--- lab1/4.c
+++ lab1/4.c
@@ -1,10 +1,10 @@
+ /*Print entire array each element in newline*/
+
 #include <stdio.h>
-int main()
-{
+int main(){
     int arr[100] = {1, 2, 3, 4, 5, 6, 7,8, 9, 10};
     int size = 10, i;
-    for(i = 0; i<size; i++)
-    {
+    for(i = 0; i<size; i++){
         printf("%d\n", arr[i]);
     }
     return 0;
