8b0f207a768e06face95d7e1a3311beee1161431
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-15 04:01:02 +0600

    Update 10.c

--- lab2/10.c
+++ lab2/10.c
@@ -1,1 +1,42 @@
+#include<stdio.h>
+int main (){
+    int arr[10]= {1,2,3,4,5,6,7,8,9,10};
+    int i, j, X;
+    int flag=0, size=10;
 
+    scanf("%d",&X);
+
+    for(i=0; i<size;i++){
+        printf("%d ",arr[i]);
+    }
+
+    printf("\n\n");
+
+    for(j=0;j<size;j++){
+
+        if(X==arr[j]){
+
+            for(i=X-1;i<=size;i++){
+                arr[i] = arr[i+1];
+            }
+            flag++;
+        }
+    }
+
+    size--;
+
+    if(flag!=1)
+    {
+        printf("element Not Found\n");
+    }
+    else
+    {
+        for(i=0;i<size;i++)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+
+
+    return 0;
+}
