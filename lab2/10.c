2062ba830cec5d4b445eb58c82d77c757a5e6c4e
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-15 04:48:30 +0600

    Update 10.c

--- lab2/10.c
+++ lab2/10.c
@@ -1,3 +1,8 @@
+/*Delete a value x from the array shift all other value to left.
+If x is not present in the given array print element not found.
+Only delete the first occurrence of the value .
+And print the whole array*/
+
 #include<stdio.h>
 int main (){
     int arr[10]= {1,2,3,4,5,6,7,8,9,10};
