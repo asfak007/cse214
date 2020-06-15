68ca45be57320bde276749214719b3b1fa23cbbe
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-15 12:12:45 +0600

    Update 6.c

--- lab5/6.c
+++ lab5/6.c
@@ -1,25 +1,29 @@
+/*  Update Kth (take input K from user) index with the value “X”
+    (take input X from user). And print the whole array. Access
+    the array using pointer */
+
 #include<stdio.h>
 int main()
 {
     int *p;
-    int i, n, m;
-    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int i, X, K;
+    int arr[100] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int size = 10;
 
-    scanf("%d",&m);
+    scanf("%d",&K);
 
-    scanf("%d",&n);
+    scanf("%d",&X);
 
     p = &arr[0];
 
-    for(i=m-1; i<size; i++){
-        *(p+i)=n;
+    for(i=K-1; i<size; i++){
+        *(p+i)=X;
         break;
     }
     printf("\n");
 
-    for(i=0; i<size; i++){
+    for(i=0; i<size; i++)
         printf("%d ", *(p+i));
-     }
+
     return 0;
 }
