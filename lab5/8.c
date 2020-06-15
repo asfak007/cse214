cc2a1c57a170dee905337b0634bd31d884cf9274
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-15 12:18:10 +0600

    Update 8.c

--- lab5/8.c
+++ lab5/8.c
@@ -1,24 +1,27 @@
+/*  Delete a value from Kth index (take input K from user) from the
+    array shift all other value to left. And print the whole array.
+    Access the array using pointer */
 
 #include<stdio.h>
 int main()
 {
     int *p;
-    int i,n;
-    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int i, K;
+    int arr[100] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int size = 10;
     
 
-    scanf("%d",&n);
+    scanf("%d",&K);
 
     p = &arr[0];
 
-    for (i=n-1; i<size; i++){
+    for (i=K-1; i<size; i++)
         *(p+i) = *(p+i+1);
-    }
+
     size--;
-    
-    for(i=0; i<size; i++){
+
+    for(i=0; i<size; i++)
         printf("%d ", *(p+i));
-    }
+
     return 0;
 }
