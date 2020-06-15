ad708286e04fa7683659a77bdd0f009ef3ebba0a
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-15 12:08:36 +0600

    Update 4.c

--- lab5/4.c
+++ lab5/4.c
@@ -1,9 +1,10 @@
+/*Print entrie array each element separated space.Access the array using pointer*/
 #include<stdio.h>
 int main()
 {
     int i;
     int *p;
-    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int arr[100] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int size = 10;
 
     p = &arr[0];
