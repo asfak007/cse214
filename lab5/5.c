987963e607612eeb23547838d0b7d4dc41d75435
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-15 12:10:31 +0600

    Update 5.c

--- lab5/5.c
+++ lab5/5.c
@@ -1,23 +1,26 @@
+/*Insert a value x(take input X from user)in array at kth (take input K from user)index and shift all other value to right.
+And print the whole array.Access the array using ponter.*/
+
 #include<stdio.h>
 int main()
 {
     int *p;
-    int i, n, m;
+    int i, x, k;
     int arr[100] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int size = 10;
 
-    scanf("%d",&n);
+    scanf("%d",&x);
 
-    scanf("%d",&m);
+    scanf("%d",&k);
 
     p = &arr[0];
 
-    for (i=size; i>=m; i--){
+    for (i=size; i>=k; i--){
         *(p+i) = *(p+i-1);
 
     size++;
     }
-    *(p+K-1) = n;
+    *(p+K-1) = x;
 
     printf("\n");
 
