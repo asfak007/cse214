197d2c0b5a43cf23e93e501cbe80b262ceefcf45
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-15 11:36:08 +0600

    Update 5.c

--- lab3/5.c
+++ lab3/5.c
@@ -1,16 +1,14 @@
+/*Calculate the summation of all elements of given array*/
 #include<stdio.h>
-int main()
-{
+int main(){
     int array[5]={1,2,3,4,5};
     int i,sum=0;
     printf("Given Array : ");
-    for(i=0;i<5;i++)
-    {
+    for(i=0;i<5;i++){
         printf("%d ",array[i]);
     }
 
-    for(i=0;i<5;i++)
-    {
+    for(i=0;i<5;i++){
         sum = sum+array[i];
     }
 
