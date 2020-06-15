6c6b17facd092f37afac93735315d6c0106ad8f1
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-15 12:16:53 +0600

    Update 7.c

--- lab5/7.c
+++ lab5/7.c
@@ -6,26 +6,26 @@
 int main()
 {
     int *p;
-    int i, X, location;
-    int array[100] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int i, X, locat;
+    int arr[100] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int size = 10;
     int tag = 0;
 
     scanf("%d",&X);
 
-    p = &array[0];
+    p = &arr[0];
 
     for(i=0; i<size; i++)
     {
         if(*(p+i)==X)
         {
-            location = i;
+            locat = i;
             tag++;
         }
     }
 
     if(tag==1)
-        printf("%d",location);
+        printf("%d",locat);
     else
         printf("-1");
 
