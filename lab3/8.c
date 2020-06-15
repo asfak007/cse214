074bbf14f38a4c0136631dbc779d8f093a82b38f
Sajal Asfak Robin <65647153+1834902575@users.noreply.github.com>
2020-06-15 11:44:53 +0600

    Update 8.c

--- lab3/8.c
+++ lab3/8.c
@@ -1,6 +1,6 @@
+/*Calculate first 92 Fibonacci number and store those in an array*/
 #include<stdio.h>
-int main()
-{
+int main(){
     int i, n,array[100];
 
     printf("How many Fibonacci numbers : ");
@@ -9,14 +9,12 @@ int main()
     array[0]=0;
     array[1]=1;
 
-    for(i=2;i<n;i++)
-        {
+    for(i=2;i<n;i++){
            array[i]=array[i-1] +array[i-2];
 
         }
 
-    for(i=0;i<n;i++)
-    {
+    for(i=0;i<n;i++){
         printf("%d\n ",array[i]);
     }
 
